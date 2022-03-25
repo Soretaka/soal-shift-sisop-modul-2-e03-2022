3cfcdf79c91062ee4652bfc1064022e9ae38e028
florentinobenedictus <florentinobenedictus@gmail.com>
2022-03-25 10:57:32 +0000

    Update Revisi soal1.c

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -28,14 +28,14 @@ void tostring(int x){
 	// fodder
 	for(int i=0; i<len; i++)out[i] = 'a';
 	while(temp--){
-	    if(x)out[temp] = '0' + x%10;
+	    if(x)out[temp] = '0' + (x%10);
 	    else out[temp] = '0' + x;
 	    x/=10;
 	}
 	out[len] = '\0';
 }
 void extractFile(){
-	
+	int status;
 	if(fork()==0){
 		char temp[100];
 		strcpy(temp, dir);
@@ -45,6 +45,7 @@ void extractFile(){
 		execv("/bin/unzip", argv);
 	}
 	else{
+		while(wait(&status) > 0);
 		char temp[100];
 		strcpy(temp, dir);
 		strcat(temp, "/gacha_gacha");
@@ -54,6 +55,7 @@ void extractFile(){
 	}
 }
 void downloadFile(){
+	int status;
 	if(fork()==0){
 		char temp[50];
 		strcpy(temp, dir);
@@ -63,12 +65,13 @@ void downloadFile(){
 		execv("/bin/wget", argv);
 	}
 	else{
+		while(wait(&status) > 0);
 		char temp[50];
 		strcpy(temp, dir);
 		//strcat(temp, "/gacha_gacha");
 		strcat(temp, "/weap.zip");
 		char *argv2[] = {"wget", "-q", "drive.google.com/uc?export=download&id=1XSkAqqjkNmzZ0AdIZQt_eWGOZ0eJyNlT", "-O", temp};
-        execv("/bin/wget", argv2);
+        	execv("/bin/wget", argv2);
 	}
 }
 void createDir(){
@@ -78,7 +81,7 @@ void createDir(){
 	if (child_id < 0) {
 		exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
   	}
-    if(child_id == 0){
+        if(child_id == 0){
 		char temp[50];
 		strcpy(temp, dir);
 		strcat(temp, "/gacha_gacha");
@@ -103,7 +106,6 @@ void createDir(){
 	}	
 }
 void saveFileName(){
-	if(fork()==0){
 		// char file names
 		DIR *d;
 		struct dirent *dirs;
@@ -114,66 +116,74 @@ void saveFileName(){
 		d = opendir(temp);
 		if (d) {
 			while ((dirs = readdir(d)) != NULL) {
+				if(strcmp(dirs->d_name, ".")==0 ||
+				   strcmp(dirs->d_name, "..")==0)continue;
 				strcpy(fileChar[charCount],dirs->d_name);
 				charCount++;
 			}
 		closedir(d);
 		}
-	}
-	else{
 		// weap file names
-		DIR *d;
-		struct dirent *dirs;
-		char temp[100];
 		strcpy(temp, dir);
 		strcat(temp, "/gacha_gacha");
 		strcat(temp, "/weapons");
 		d = opendir(temp);
 		if (d) {
 			while ((dirs = readdir(d)) != NULL) {
+				if(strcmp(dirs->d_name, ".")==0 ||
+				   strcmp(dirs->d_name, "..")==0)continue;
 				strcpy(fileWeap[weapCount],dirs->d_name);
 				weapCount++;
 			}
 		closedir(d);
 		}
-	}
+		printf("---------------\nJumlah Char: %d\nJumlah Weap: %d\nJika salah satu jumlah char/weap 0 file corrupt ketika download!\n---------------\n", charCount, weapCount);
 }
 void saveDataValues(){
-	if(fork()==0){
+	char buffer[4096];
+	if(charCount){
 		// save char values to arr
 		FILE *fp;
-		char buffer[1024];
 		struct json_object *parsed_json;
 		struct json_object *name;
 		struct json_object *rarity;
 		for(int i=0; i<charCount; i++){
-			fp = fopen(fileChar[i], "r");
-			fread(buffer, 1024, 1, fp);
+			char temp[100];
+			strcpy(temp, "");
+			strcat(temp, dir);
+			strcat(temp, "/gacha_gacha/characters/");
+			strcat(temp, fileChar[i]);
+			fp = fopen(temp, "r");
+			fread(buffer, 4096, 1, fp);
 			fclose(fp);
-			
 			parsed_json = json_tokener_parse(buffer);
-			
+
 			json_object_object_get_ex(parsed_json, "name", &name);
 			json_object_object_get_ex(parsed_json, "rarity", &rarity);
-			
+
 			strcpy(nameChar[i], json_object_get_string(name));
 			strcpy(rarityChar[i], json_object_get_string(rarity));
 		}
 	}
-	else{
+	if(weapCount){
 		// save weap values to arr
 		FILE *fp;
-		char buffer[1024];
 		struct json_object *parsed_json;
 		struct json_object *name;
 		struct json_object *rarity;
 		for(int i=0; i<weapCount; i++){
-			fp = fopen(fileWeap[i], "r");
-			fread(buffer, 1024, 1, fp);
+			char temp[100];
+			strcpy(temp, "");
+			strcat(temp, dir);
+			strcat(temp, "/gacha_gacha/weapons/");
+			strcat(temp, fileWeap[i]);
+
+			fp = fopen(temp, "r");
+			fread(buffer, 4096, 1, fp);
 			fclose(fp);
-			
+
 			parsed_json = json_tokener_parse(buffer);
-			
+
 			json_object_object_get_ex(parsed_json, "name", &name);
 			json_object_object_get_ex(parsed_json, "rarity", &rarity);
 			
@@ -181,21 +191,11 @@ void saveDataValues(){
 			strcpy(rarityWeap[i], json_object_get_string(rarity));
 		}
 	}
+printf("PARSING DONE\n");
 }
 void dataInit(){
-	pid_t child_id;
-	child_id = fork();
-	int status;
-	if (child_id < 0) {
-		exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-  	}
-	if(child_id == 0){
-		saveFileName();
-	}
-	else{
-		while(wait(&status) > 0);
-		saveDataValues();
-	}
+	saveFileName();
+	saveDataValues();
 }
 void resetResult(){
 	strcpy(gachaRes, "");
@@ -203,136 +203,134 @@ void resetResult(){
 void changeFolder(){
 	char folderName[30];
 	int status;
+	printf("Ganti folder!\n");
+	strcpy(folderPath, "");
+	strcat(folderPath, dir);
+	strcat(folderPath, "/gacha_gacha");
+		
+	strcpy(folderName, "");
+	strcat(folderName, "/total_gacha_");
+	tostring(numgacha);
+	strcat(folderName, out);
+		
+	strcat(folderPath, folderName);
 	if(fork()==0){
-		strcpy(folderPath, "");
-		strcat(folderPath, dir);
-		strcat(folderPath, "/gacha_gacha");
-		
-		strcpy(folderName, "");
-		strcat(folderName, "/total_gacha_");
-		tostring(numgacha);
-		strcat(folderName, out);
-		
-		strcat(folderPath, folderName);
 		char *argv[] = {"mkdir", "-p", folderPath, NULL};
 		execv("/bin/mkdir", argv);
 	}
 	else {
 		while(wait(&status) > 0);
+		printf("folder terbuat di: %s\n", folderPath);
 		//strcat(folderPath, folderName);
 	}
 }
 void changeTxt(){
 	char txtName[40];
 	int status;
-	if(fork()==0){
-		strcpy(txtPath, "");
-		strcat(txtPath, folderPath);
-		time_t seconds;
-		struct tm *timeStruct;
-		seconds = time(NULL);
-		// change localtime
-		timeStruct = localtime(&seconds);
-		
-		strcpy(txtName, "");
-		strcat(txtName, "/");
-		tostring(timeStruct->tm_hour);
-		if(timeStruct->tm_hour > 9)strcat(txtName, out);
-		else {
-			strcat(txtName, "0");
-			tostring(timeStruct->tm_hour);
-			strcat(txtName, out);
-		}
-		strcat(txtName, ":");
-		tostring(timeStruct->tm_min);
-		if(timeStruct->tm_min > 9)strcat(txtName, out);
-		else {
-			strcat(txtName, "0");
-			tostring(timeStruct->tm_min);
-			strcat(txtName, out);
-		}
-		strcat(txtName, ":");
-		tostring(timeStruct->tm_sec);
-		if(timeStruct->tm_sec > 9)strcat(txtName, out);
-		else{
-			strcat(txtName, "0");
-			tostring(timeStruct->tm_sec);
-			strcat(txtName, out);
-		}
-		
-		strcat(txtName, "_gacha_");
-		tostring(numgacha);
-		strcat(txtName, out);
-		strcat(txtName, ".txt");
-		char buff[256];
-		strcat(txtPath, txtName);
-		//snprintf(buff, sizeof buff, "%s", txtPath);
-		FILE * fp;
-		fp = fopen (txtPath, "w+");
-		fprintf(fp, "%s\n", txtPath);
-		fclose(fp);
-	}
+	strcpy(txtPath, "");
+	strcat(txtPath, folderPath);
+	time_t seconds;
+	struct tm *timeStruct;
+	seconds = time(NULL);
+	// change localtime
+	timeStruct = localtime(&seconds);
+	
+	strcpy(txtName, "");
+	strcat(txtName, "/");
+	tostring(timeStruct->tm_hour);
+	if(timeStruct->tm_hour > 9)strcat(txtName, out);
 	else {
-		while(wait(&status) > 0);
-		// wait 1 sec
-		sleep(1);
+		strcat(txtName, "0");
+		tostring(timeStruct->tm_hour);
+		strcat(txtName, out);
 	}
+	strcat(txtName, ":");
+	tostring(timeStruct->tm_min);
+	if(timeStruct->tm_min > 9)strcat(txtName, out);
+	else {
+		strcat(txtName, "0");
+		tostring(timeStruct->tm_min);
+		strcat(txtName, out);
+	}
+	strcat(txtName, ":");
+	tostring(timeStruct->tm_sec);
+	if(timeStruct->tm_sec > 9)strcat(txtName, out);
+	else{
+		strcat(txtName, "0");
+		tostring(timeStruct->tm_sec);
+		strcat(txtName, out);
+	}
+
+	strcat(txtName, "_gacha_");
+	tostring(numgacha);
+	strcat(txtName, out);
+	strcat(txtName, ".txt");
+	char buff[256];
+	strcat(txtPath, txtName);
+	FILE * fp;
+	fp = fopen (txtPath, "a+");
+	fprintf(fp, "Path Text: %s\n\n", txtPath);
+	fclose(fp);
+
+printf("txt terbuat di: %s\n", txtPath);
+	// wait 1 sec
+	sleep(1);
 }
 void charToTxt(int RNGchar){
 	int status;
 	char charGet[50];
-	if(fork()==0){
-		strcpy(charGet, "");
-		tostring(numgacha);
-		strcat(charGet, out);
-		strcat(charGet, "_characters_");
-		strcat(charGet, rarityChar[RNGchar]);
-		strcat(charGet, "_");
-		strcat(charGet, nameChar[RNGchar]);
-		strcat(charGet, "_");
-		tostring(primo);
-		strcat(charGet, out);
-		strcat(charGet, "\n");
+	strcpy(charGet, "");
+	tostring(numgacha);
+	strcat(charGet, out);
+	strcat(charGet, "_characters_");
+	strcat(charGet, rarityChar[RNGchar]);
+	strcat(charGet, "_");
+	strcat(charGet, nameChar[RNGchar]);
+	strcat(charGet, "_");
+	tostring(primo);
+	strcat(charGet, out);
+	strcat(charGet, "\n");
 		
         FILE *out=fopen(txtPath,"a+");
-		fprintf(out, "%s\n", charGet);
+	fprintf(out, "%s\n", charGet);
         fclose(out);
-	}
-	else while(wait(&status) > 0);
 }
 void weapToTxt(int RNGweap){
 	int status;
 	char weapGet[50];
+	strcpy(weapGet, "");
+	tostring(numgacha);
+	strcat(weapGet, out);
+	strcat(weapGet, "_weapons_");
+	strcat(weapGet, rarityWeap[RNGweap]);
+	strcat(weapGet, "_");
+	strcat(weapGet, nameWeap[RNGweap]);
+	strcat(weapGet, "_");
+	tostring(primo);
+	strcat(weapGet, out);
+	strcat(weapGet, "\n");
+	
+	FILE *out=fopen(txtPath,"a+");
+	fprintf(out, "%s\n", weapGet);
+	fclose(out);
+}
+void zipFile(){
+	int status;
 	if(fork()==0){
-		strcpy(weapGet, "");
-		tostring(numgacha);
-		strcat(weapGet, out);
-		strcat(weapGet, "_weapons_");
-		strcat(weapGet, rarityWeap[RNGweap]);
-		strcat(weapGet, "_");
-		strcat(weapGet, nameWeap[RNGweap]);
-		strcat(weapGet, "_");
-		tostring(primo);
-		strcat(weapGet, out);
-		strcat(weapGet, "\n");
-		
-		FILE *out=fopen(txtPath,"w+");
-		fputs(weapGet,out);
-		fclose(out);
+		char *argv[] = {"zip","-r","not_safe_for_wibu.zip","-P", "satuduatiga", "gacha_gacha", NULL};
+		execv("/bin/zip", argv);
+	}
+	else while(wait(&status) > 0); 
+}
+void deleteFile(){
+	int status;
+	if(fork()==0){
+		char *argv[] = {"rm", "-r", "gacha_gacha", NULL};
+		execv("/bin/rm", argv);
 	}
 	else while(wait(&status) > 0);
 }
-void zipFile(){
-	char *argv[] = {"zip","-r","not_safe_for_wibu.zip","-P", "satuduatiga", "gacha_gacha", NULL};
-	execv("/bin/zip", argv); 
-}
-void deleteFile(){
-	char *argv[] = {"rm", "-r", "gacha_gacha", NULL};
-	execv("/bin/rm", argv);
-}
 int main(int argc, char* argv[]){
-
-
 
 	clock_t begin = clock();
 	time_t seconds;
@@ -340,84 +338,69 @@ int main(int argc, char* argv[]){
 	seconds = time(NULL);
 	// change localtime
 	timeStruct = localtime(&seconds);
-	timeinfo->tm_mday,
-            timeinfo->tm_mon + 1, timeinfo->tm_year + 1900,
-            timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-	while(true){
-		printf("%d %d %d %d:%d:%d\n", timeStruct->tm_mday,
+	while(1){
+		printf("Time now: %d %d %d %d:%d:%d, wait anniversary!\n", timeStruct->tm_mday,
             timeStruct->tm_mon + 1, timeStruct->tm_year + 1900,
             timeStruct->tm_hour, timeStruct->tm_min, timeStruct->tm_sec);
-		
-		if(timeStruct->tm_mday==30 && timeStruct->tm_mon + 1 == 3 && timeStruct->tm_hour == 4 && timeStruct->tm_min == 44){
+		if(timeStruct->tm_mday==30 && timeStruct->tm_mon + 1 == 3 && timeStruct->tm_hour == 4 && timeStruct->tm_min == 44)break;
+	}
 
 
 
+	pid_t child_id;
 
-					pid_t child_id;
-
-					strcpy(dir, "");
-					strcpy(dir, "/home/user");
-					// default
-					strcpy(folderPath, "");
-					int status;
-					child_id = fork();
-					if (child_id < 0) {
-						exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-					}
-					if(child_id == 0){
-						createDir();
-					}
-					else{
-						while(wait(&status) > 0);
-						pid_t child_id2;
-						//status = NULL;
-						child_id2 = fork();
-						if (child_id2 < 0) {
-						exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-						}
-						if(child_id2 == 0){
-							dataInit();
-						}
-						else{
-							while(wait(&status) > 0);
-							primo = 79000, numgacha = 0;
-							while(primo >= 160){
-								primo-=160;
-								resetResult();
-								if(numgacha % 90 == 0)changeFolder();
-								if(numgacha % 10 == 0)changeTxt();
-								numgacha++;
-								if(numgacha % 2){
-									charToTxt(rand() % charCount);
-								}
-								else{
-									weapToTxt(rand() % weapCount);
-								}
-							}
-							// gacha done
-						}
-			}
-			clock_t end = clock();
-			if((end-begin)/CLOCKS_PER_SEC < 1800){
-				sleep(1800 - ((end-begin)/CLOCKS_PER_SEC));
-				printf("Wait %ld seconds", 1800 - ((end-begin)/CLOCKS_PER_SEC));
-			}
-			pid_t child_id3;
-			//status = NULL;
-			child_id3 = fork();
-			if (child_id3 < 0) {
-				exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-			}
-			if(child_id3 == 0){
-				zipFile();
+	strcpy(dir, "");
+	strcpy(dir, "/home/florentino"); //ganti sesuai nama user
+	// default
+	strcpy(folderPath, "");
+	int status;
+	child_id = fork();
+	if (child_id < 0) {
+		exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+	}
+	if(child_id == 0){
+		createDir();
+	}
+	else{
+		while(wait(&status) > 0);
+		dataInit();
+		primo = 79000, numgacha = 0;
+		while(primo >= 160){
+			printf("Current primo: %d\n", primo);
+			primo-=160;
+			if((numgacha % 90) == 0)changeFolder();
+			if((numgacha % 10) == 0)changeTxt();
+			numgacha++;
+			if(numgacha % 2){
+				charToTxt(rand() % charCount);
 			}
 			else{
-				while(wait(&status) > 0);
-				deleteFile();
-				break;
+				weapToTxt(rand() % weapCount);
 			}
-			
 		}
+		printf("Primo habis, top up lagi ngab\n");
+		printf("Total laksek: %d", numgacha);
+		// gacha done
 		
+		clock_t end = clock();
+		if((end-begin)/CLOCKS_PER_SEC < 10800){
+			printf("Wait %ld seconds..\n", 10800 - ((end-begin)/CLOCKS_PER_SEC));
+			sleep(10800 - ((end-begin)/CLOCKS_PER_SEC));
+		}
+		pid_t child_id3;
+		//status = NULL;
+		child_id3 = fork();
+		if (child_id3 < 0) {
+			exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+		}
+		if(child_id3 == 0){
+			zipFile();
+		}
+		else{
+			while(wait(&status) > 0);
+			printf("Aduh ga dapet SSR :(\n");
+			deleteFile();
+		}			
+	}
 	return 0;
 }
